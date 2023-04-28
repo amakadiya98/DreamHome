ac0e03b599256429bd41d536ab7fe41fd2fac2a3
sagar-closedigit <sagar@closedigit.com>
2023-04-28 15:02:53 +0530

    home design complete with responsive

--- src/components/Button.js
+++ src/components/Button.js
@@ -23,4 +23,14 @@ const StyledButton = styled(Button)`
     border: 1px solid transparent;
     border-radius: 5px;
   }
+
+  @media screen and (max-width: 599px) {
+    font-size: 16px;
+    line-height: 20px;
+  }
+  @media screen and (max-width: 400px) {
+    font-size: 14px;
+    line-height: 18px;
+    padding: 7px 14px;
+  }
 `;
